0f4db23b88db65db5090fd786007f23cf7b7758c
Michael Schumann <ms@itc-ms.de>
2016-12-05 13:25:16 +0100

    A few small improvements, German language messages removed

--- gitFileDates.dpr
+++ gitFileDates.dpr
@@ -2,37 +2,39 @@ program gitFileDates;
 
 {$APPTYPE CONSOLE}
 {$R *.res}
+(* ****************************************************************************************
+  gitFileDates
 
-  (*****************************************************************************************
-   gitFileDates
+  (c) Michael Schumann
+  I share this code with anyone who can use it without any warranty for any purpose.
+  Of course no attibution is required if its used.
 
-   (c) Michael Schumann
-   I share this code with anyone who can use it without any warranty for any purpose.
-   Of course no attibution is required if its used.
+  Save and restore last file write attribite for those who use git under Windows.
+  Created because I use to check in all required files for building the application
+  including DLLs. As some update mechanisms rely on file dates it is important to restore
+  them before building the package.
 
-   Save and restore last file write attribite for those who use git under Windows.
-   Created because I use to check in all required files for building the application
-   including DLLs. As some update mechanisms rely on file dates it is important to restore
-   them before building the package.
+  How to use
+  If using TortoiseGit just create a Tortoise hook (in Tortoise settings) calling this program
+  "on start commit" like this:
 
-   How to use
-   If using TortoiseGit just create a Tortoise hook (in Tortoise settings) calling this program
-   "on start commit" like this:
+  gitFileDates /dir <root path to source> /save
 
-   gitFileDates /dir <root path to source> /save
+  creates the file .gitfileattr with all file last modified informations, must be checked in.
 
-   creates the file .gitfileattr with all file last modified informations, must be checked in.
+  When building the project with a runner (gitlab-ci) include one stage after checkout like
+  this:
 
-   When building the project with a runner (gitlab-ci) include one stage after checkout like
-   this:
+  gitFileDates /dir <root path to source> /apply
 
-   gitFileDates /dir <root path to source> /apply
+  applies the timestamps saved in .gitfileattr to the source tree.
 
-   applies the timestamps saved in .gitfileattr to the source tree.
+  Timestamps of folders will not be modified.
 
-   Timestamps of folders will not be modified.
+  The code is more or less quick and dirty. No try/finally for destroying objects as it quits
+  anyway and not memory traces are left.
 
-   *****************************************************************************************)
+  **************************************************************************************** *)
 
 uses
   Classes,
@@ -42,9 +44,8 @@ uses
 
 var
   sl, sl1: TStringList;
-  i: integer;
-  s, CurDir: string;
-  fage: integer;
+  i, j, fage: integer;
+  s, curdir: string;
 
 function PathRelativePathTo(pszPath: PChar; pszFrom: PChar; dwAttrFrom: DWORD; pszTo: PChar; dwAtrTo: DWORD): LongBool;
   stdcall; external 'shlwapi.dll' name 'PathRelativePathToW';
@@ -75,42 +76,48 @@ begin
       writeln('Usage: gitFileDates /save or /apply');
       exit;
     end;
-    CurDir := getCurrentDir;
+    curdir := getCurrentDir;
     if FindCmdLineSwitch('dir') then
-      FindCmdLineSwitch('dir', CurDir, true, [clstValueNextParam]);
-    CurDir := IncludeTrailingPathDelimiter(CurDir);
+      FindCmdLineSwitch('dir', curdir, true, [clstValueNextParam]);
+    curdir := IncludeTrailingPathDelimiter(curdir);
     if FindCmdLineSwitch('save') then
     begin
       sl := TStringList.create;
       sl1 := TStringList.create;
-      advBuildFileList(CurDir + '*.*', faAnyFile, sl, amSuperSetOf, [flRecursive, flFullNames]);
+      advBuildFileList(curdir + '*.*', faAnyFile, sl, amSuperSetOf, [flRecursive, flFullNames]);
       for i := 0 to sl.count - 1 do
-      begin
-        if (extractFileName(sl[i]) <> '.') and (extractFileName(sl[i]) <> '..') and (FileAge(sl[i]) > -1) then
-          sl1.add(AbsToRel(sl[i], CurDir) + ':' + intToStr(FileAge(sl[i])));
-      end;
-      sl1.saveToFile(CurDir + '.gitfileattr');
+        if pos('\.git\', sl[i]) = 0 then
+        begin
+          if (extractFileName(sl[i]) <> '.') and (extractFileName(sl[i]) <> '..') and (FileAge(sl[i]) > -1) then
+            sl1.add(AbsToRel(sl[i], curdir) + ':' + intToStr(FileAge(sl[i])));
+        end;
+      sl1.saveToFile(curdir + '.gitfileattr');
       sl.free;
       sl1.free;
     end;
     if FindCmdLineSwitch('apply') then
     begin
-      if not fileExists(CurDir + '.gitfileattr') then
+      if not fileExists(curdir + '.gitfileattr') then
       begin
-        writeln('Datei .gitfileattr nicht gefunden');
+        writeln('File .gitfileattr not found.');
         exit;
       end;
       sl := TStringList.create;
       sl1 := TStringList.create;
       sl1.delimiter := ':';
       sl1.strictDelimiter := true;
-      sl.loadFromFile(CurDir + '.gitfileattr');
+      sl.loadFromFile(curdir + '.gitfileattr');
       for i := 0 to sl.count - 1 do
       begin
         sl1.delimitedText := sl[i];
-        s := RelToAbs(sl1[0], CurDir);
-        fage := StrToInt(sl1[1]);
-        FileSetDate(s, fage);
+        s := RelToAbs(sl1[0], curdir);
+        if (pos('\.git\', s) = 0) and fileExists(s) then
+        begin
+          fage := StrToInt(sl1[1]);
+          j := FileSetDate(s, fage);
+          if j <> 0 then
+            writeln('Error ' + intToStr(j) + ' file ' + s);
+        end;
       end;
       sl1.free;
       sl.free;
